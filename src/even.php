81f8d02fc4010499243e2faf52d4eb25352f6c02
dzadorin <zadorin7zadorin@yandex.ru>
2025-02-21 22:02:23 +0400

    work first game

---
+++ src/even.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BrainGames\even;
+
+use function cli\line;
+use function cli\prompt;
+
+line("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+$response_user = prompt("Question: 15");
+if ($response_user === 'no') {
+    line("Correct");
+    $response_user = prompt("Question: 6");
+
+    if ($response_user === 'yes') {
+        line("Correct");
+        $response_user = prompt("Question: 7");
+    } else {
+        line("'no' is wrong answer ;(. Correct answer was 'yes'.");
+        line("Let's try again, %s!", $name);
+        exit();
+    }
+
+    if ($response_user === 'no') {
+        line("Correct");
+        line("Congratulations, %s!", $name);
+    } else {
+        line("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        line("Let's try again, %s!", $name);
+        exit();
+    }
+
+} else {
+    line("'yes' is wrong answer ;(. Correct answer was 'no'.");
+    line("Let's try again, %s!", $name);
+    exit();
+}
