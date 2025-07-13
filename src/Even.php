b0bbfd741d4c0cbf5510389ff232588761d8c1bb
Zadorin Danil <zadorin7zadorin@yandex.ru>
2025-07-13 20:48:01 +0400

    work

--- src/Even.php
+++ src/Even.php
@@ -1,48 +1,43 @@
 <?php
 
-namespace BrainGames\Even;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use function cli\line;
 use function cli\prompt;
 
-line('Answer "yes" if the number is even, otherwise answer "no".');
-$answer = prompt('Question 15');
-line("Your answer: %s", $answer);
+function isEven() {
+    $countIteration = 1;
+    $flag = true;
+    $randomNum = mt_rand(1, 100);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    $answer = prompt("Question {$randomNum}");
 
-if ($answer === 'no') {
-    line("Correct!");
-    $answer = prompt('Question 6');
-    line("Your answer: %s", $answer);
+    while($flag && $countIteration >= 0) {
+        iteration($randomNum, $name, $answer, $flag);
+        $countIteration--;
 
-    if ($answer === 'yes') {
-        line("Correct!");
-        $answer = prompt('Question 7');
-        line("Your answer: %s", $answer);
-
-        if ($answer === 'no') {
-            line("Correct!");
-            line("Congratulations, {$name}!");
-        } elseif ($answer === 'yes') {
-            line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {$name}'");
-        } else {
-            line("{$answer} is not correct!");
-        }
-
-    } elseif ($answer === 'no') {
-        line("'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {$name}'");
-    } else {
-        line("{$answer} is not correct!");
+	if ($flag && $countIteration === 0) {
+            line("Configuration, {$name}!");
+    	}
     }
+
 }
 
+function iteration(&$randomNum, &$name, &$answer, &$flag) {
+    if ($answer === 'yes' && $randomNum % 2 === 0 || $answer === 'no' && $randomNum % 2 !== 0) {
+        line("Your answer: %s", $answer);
+	line("Correct!");
+        $randomNum = mt_rand(1, 100);
+        $answer = prompt("Question {$randomNum}");
+    }
 
-elseif ($answer === 'yes') {
-    line("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {$name}'");
-}
-
-
-else {
-    line("{$answer} is not correct!");
+    else {
+        if($randomNum % 2 === 0) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {$name}!");
+        } else {
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {$name}!");
+        }
+        $flag = false;
+    }
 }
