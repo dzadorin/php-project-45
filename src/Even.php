f4ff163df65d57ab6fcce02262dbf4fc5fb590cf
Zadorin Danil <zadorin7zadorin@yandex.ru>
2025-07-13 21:40:55 +0400

    finished 5

--- src/Even.php
+++ src/Even.php
@@ -4,39 +4,43 @@ use function cli\line;
 use function cli\prompt;
 
 function isEven() {
-    $countIteration = 1;
+    $countIteration = 3;
     $flag = true;
-    $randomNum = mt_rand(1, 100);
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+    line("Hello, {$name}");
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $answer = prompt("Question {$randomNum}");
 
-    while($flag && $countIteration >= 0) {
-        iteration($randomNum, $name, $answer, $flag);
-        $countIteration--;
+    while($flag && $countIteration > 0) {
+	$randomNum = mt_rand(1, 100);
+	$answer = prompt("Question {$randomNum}");
+	line("Your answer: %s", $answer);
 
-	if ($flag && $countIteration === 0) {
-            line("Configuration, {$name}!");
-    	}
+	iteration($randomNum, $name, $answer, $flag);
+
+	if ($flag) {
+            line("Correct!");
+            $countIteration--;
+        } else {
+            break;
+        }
     }
 
+    if ($flag) {
+        line("Congratulations, {$name}!");
+    } else {
+        line("Let's try again, {$name}!");
+    }
 }
 
 function iteration(&$randomNum, &$name, &$answer, &$flag) {
-    if ($answer === 'yes' && $randomNum % 2 === 0 || $answer === 'no' && $randomNum % 2 !== 0) {
-        line("Your answer: %s", $answer);
-	line("Correct!");
-        $randomNum = mt_rand(1, 100);
-        $answer = prompt("Question {$randomNum}");
-    }
-
-    else {
-        if($randomNum % 2 === 0) {
-            line("'{$answer}' is wrong answer ;(. Correct answer was 'yes'. \nLet's try again, {$name}!");
+    if (($answer === 'yes' && $randomNum % 2 === 0) || ($answer === 'no' && $randomNum % 2 !== 0)) {
+        return;
+    } else {
+        if ($randomNum % 2 === 0) {
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'yes'.");
         } else {
-            line("'{$answer}' is wrong answer ;(. Correct answer was 'no'. \nLet's try again, {$name}!");
+            line("'{$answer}' is wrong answer ;(. Correct answer was 'no'.");
         }
         $flag = false;
     }
